469011f0f1f7df24638c76a90b3d4e1edec5be0f
Aleksey <shvejk@bk.ru>
2021-01-22 09:28:28 +0300

    Cleans code and adds some comments

--- tryme.py
+++ tryme.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python3
 
+import re
+
 source = ""
 data = list()
 blankFlag = False
@@ -7,27 +9,29 @@ blankFlag = False
 with open("qsource") as f:
     source = f.read()
 
-for i, l in enumerate(source.splitlines()):
+for l in source.splitlines():
 
-    if i > 20: # remove enumeration from the final script
-        break
-
+    # Strings with '№' sign shows the beginning of the new chapter
     if l.find('№') > -1:
         data.append([])
 
     else:
 
         if not l:
-
             blankFlag = True
 
         else:
 
+            # Blank line preceding the one with content signifies the
+            # beginning of the new block; blankFlag in false allows to
+            # distinguish a question from the options.
             if blankFlag:
-                data[len(data) - 1].append(l)
+                data[len(data) - 1].append(
+                        re.sub('^\s+\d+\.\s+', '', l).capitalize())
                 data[len(data) - 1].append(list())
             else:
-                data[len(data) - 1][len(data[len(data) - 1]) - 1].append(l)
+                data[len(data) - 1][len(data[len(data) - 1]) - 1].append(
+                        re.sub('^\s+\d+\.\s+', '', l))
 
             blankFlag = False
 
